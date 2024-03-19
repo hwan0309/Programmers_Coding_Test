79831fdd62755a221c4e6b9ae0c3a2a4b598a277
hwan0309 <protmxk@naver.com>
2024-03-19 18:00:50 +0900

    new commit

--- Coding_Test_01.java
+++ Coding_Test_01.java
@@ -3,7 +3,10 @@ package Coding_Test_Easy;
 import java.util.Scanner;
 
 public class Coding_Test_01 {
-
+	
+	// A + B
+	//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	
 	public static void main(String[] args) {
 		
 	Scanner sc = new Scanner(System.in);
--- Coding_Test_02.java
+++ Coding_Test_02.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Coding_Test_02 {
 
+	
+	//사칙연산
+	//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
