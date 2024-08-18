59d6713c8d4bab32c352d17a8dbcbd4109b5a84d
jihwan Yoon <154872340+hwan0309@users.noreply.github.com>
2024-08-18 20:10:10 +0900

    Add files via upload

---
+++ 세로 읽기.py
@@ -0,0 +1,7 @@
+# 문자열 my_string과 두 정수 m, c가 주어집니다. my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+
+def solution(my_string, m, c):
+    tmp = [my_string[i:i+m] for i in range(0, len(my_string), m)]
+    answer = "".join([tmp[i] [c-1] for i in range(0,len(tmp))])
+    return answer
+
